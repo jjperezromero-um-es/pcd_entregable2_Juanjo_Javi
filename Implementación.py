b78beab5f582e8d4d8acf6c2fd931620ab5227d1
j-ruizchicoy-um-es <j.ruizchicoy@um.es>
2024-05-05 15:31:27 +0200

    modificaciones diagrama e implementacion

--- Implementación.py
+++ Implementación.py
@@ -71,8 +71,8 @@ class Operador(Observer):
         self.manejador = manejador
 
     def update(self, data):
-        print(f"\n{'-' * 50}")
-        print(f"Timestamp: {data[0]} - Temperatura actual: {data[1]}°C")
+        print(f"\n{'-' * 80}")
+        print(f"Timestamp: {data[0]}. Temperatura actual: {data[1]}°C")
         self.manejador.handle_request(data)
 
 # Clase encargada de manejar las solicitudes de datos y aplicar las estrategias de cálculo.
@@ -88,50 +88,53 @@ class Manejador:
         self.verificar_umbral(temperature)
         self.comprobar_aumento_rapido()
 
-    def verificar_umbral(self, temperatura_actual, umbral=25):
+    def verificar_umbral(self, temperatura_actual, umbral=30):
         if temperatura_actual > umbral:
             print(f"Alerta: La temperatura {temperatura_actual}°C supera el umbral de {umbral}°C.")
 
+    #funcion para comprobar si ha aumentado mas de 10 grados en los ultimos 30 segundos
     def comprobar_aumento_rapido(self):
         if len(self.temperaturas) > 1:
             ultimo_ts, ultima_temp = self.temperaturas[-1]
+            #filtramos las mediciones de los ultimos 30 segundos
             mediciones_recientes = list(filter(lambda x: (ultimo_ts - x[0]).seconds <= 30, self.temperaturas[:-1]))
             if any(ultima_temp - temperatura > 10 for _, temperatura in mediciones_recientes):
                 print("Alerta: Incremento de temperatura > 10°C en los últimos 30 segundos.")
 
-# Clase abstracta para definir estrategias de cálculo de estadísticas sobre las temperaturas.
-# Patrón de diseño Strategy
+#clase abstracta que usaremos para definir las estrategias de calculo
+#patrón de diseño Strategy
 class EstrategiaCalculo(ABC):
     @abstractmethod
     def calcular(self, temperaturas):
         pass
 
-# Implementaciones concretas de la estrategia de cálculo.
+#A continuacion las implementaciones de cada estrategia de calculo que tendra el sistema
 class EstrategiaMediaDesviacion(EstrategiaCalculo):
     def calcular(self, temperaturas):
         if len(temperaturas) >= 2:
-            temps = [temp for _, temp in temperaturas]
+            #temperaturas en una lista (cojemos x[1] ya que x es una tupla (timestamp,temp))
+            temps = list(map(lambda x: x[1], temperaturas))
             print(f"Media: {mean(temps):.2f}, Desviación estándar: {stdev(temps):.2f}")
 
 class EstrategiaCuantiles(EstrategiaCalculo):
     def calcular(self, temperaturas):
         if len(temperaturas) >= 2:
-            temps = [temp for _, temp in temperaturas]
+            temps = list(map(lambda x: x[1], temperaturas))
             print(f"Cuantiles: {quantiles(temps)}")
 
 class EstrategiaMaxMin(EstrategiaCalculo):
     def calcular(self, temperaturas):
         if temperaturas:
-            temps = [temp for _, temp in temperaturas]
+            temps = list(map(lambda x: x[1], temperaturas))
             print(f"Máximo: {max(temps)}, Mínimo: {min(temps)}")
 
 if __name__ == '__main__':
-    estrategia = input("Seleccione la estrategia de cálculo (1: Media y Desviación, 2: Cuantiles, 3: Máximo y Mínimo): ")
-    if estrategia == '1':
+    estrategia = int(input("Seleccione la estrategia de cálculo (1: Media y Desviación, 2: Cuantiles, 3: Máximo y Mínimo): "))
+    if estrategia == 1:
         estrategia = EstrategiaMediaDesviacion()
-    elif estrategia == '2':
+    elif estrategia == 2:
         estrategia = EstrategiaCuantiles()
-    elif estrategia == '3':
+    elif estrategia == 3:
         estrategia = EstrategiaMaxMin()
     else:
         raise ValueError("Estrategia inválida. Escriba 1, 2 o 3.")
